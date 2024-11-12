bbca5dc0e4fcb986cc720bbb6dac87422655e4a9
aliii390 <alli.said.errahmani@gmail.com>
2024-11-12 12:32:17 +0100

    ADD: entrain de faire l'exo8

---
+++ exo8/script.js
@@ -0,0 +1,18 @@
+// Objectif : Utiliser des boucles et des conditions pour trouver une valeur.
+
+// Saisissez une liste de 5 nombres.
+// Parcoure cette liste pour déterminer et afficher le plus grand nombre.
+
+
+const nombre = [1, 2, 3, 4, 5];
+const test = 0
+
+// for(let i = 0; i < nombre.length; i++){
+ 
+     
+// }
+
+
+if(test< nombre.length){
+    console.log(Math.max(nombre));
+}
