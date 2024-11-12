32fae5e2dc4a6843930834c836c6c3b6ab8b1b55
aliii390 <alli.said.errahmani@gmail.com>
2024-11-12 09:58:46 +0100

    ADD: fin de l'exo4

---
+++ exo4/script.js
@@ -0,0 +1,10 @@
+// Objectif : Utiliser des conditions simples et une boucle.
+
+// Affiche tous les nombres pairs de 1 à 20 dans la console.
+
+
+for (let i = 2; i <= 20; i++) {
+    if (i % 2 == 0) {
+      alert( i );
+    }
+  }
